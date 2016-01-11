64b0a1b823b4273174a44c4df35db6e4a236e469
Zygimantas Benetis <benetis.zygimantas@gmail.com>
2016-01-11 23:53:56 +0200

    loading app with create nw and delete

--- src/main/resources/static/run.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by zygis on 1/7/2016.
- */
--- src/main/resources/static/src/App.js
+++ src/main/resources/static/src/App.js
@@ -4,10 +4,11 @@ var client = clientF();
 import follow from './follow';
 
 export class UserApi extends Component {
+
     constructor(props) {
         super(props);
-        this.state = { users: []};
-        this.state.pageSize = 2;
+        this.state = { attributes: [], pageSize: 2, links: {}};
+        this.state.users = [];
     }
 
     componentDidMount() {
@@ -35,23 +36,110 @@ export class UserApi extends Component {
         });
     }
 
+    onCreate(newUser) {
+        follow(client, ['users']).then(userCollection => {
+            return client({
+                method: 'POST',
+                path: userCollection.entity._links.self.href,
+                entity: newUser,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }).then(response => {
+            return follow(client, root, [
+                {rel: 'users', params: {'size': this.state.pageSize}}]);
+        }).done(response => {
+            this.onNavigate(response.entity._links.last.href);
+        });
+    }
+    onNavigate(navUri) {
+        client({method: 'GET', path: navUri}).done(userCollection => {
+            this.setState({
+                users: userCollection.entity._embedded.users,
+                attributes: this.state.attributes,
+                pageSize: this.state.pageSize,
+                links: userCollection.entity._links
+            });
+        });
+    }
+
+    onDelete(user) {
+        client({method: 'DELETE', path: user._links.self.href}).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        });
+    }
+
+    updatePageSize(pageSize) {
+        if (pageSize !== this.state.pageSize) {
+            this.loadFromServer(pageSize);
+        }
+    }
+
     render() {
         return (
-            <UsersList users={this.state.users}/>
+            <div>
+                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+                <UsersList users={this.state.users}
+                           links={this.state.links}
+                           pageSize={this.state.pageSize}
+                           onNavigate={this.onNavigate}
+                           onDelete={this.onDelete}
+                           updatePageSize={this.updatePageSize}/>
+            </div>
         );
     }
 }
 
 export class UsersList extends Component {
+    handleInput(e) {
+        e.preventDefault();
+        var pageSize = React.findDOMNode(this.refs.pageSize).value;
+        if (/^[0-9]+$/.test(pageSize)) {
+            this.props.updatePageSize(pageSize);
+        } else {
+            React.findDOMNode(this.refs.pageSize).value =
+                pageSize.substring(0, pageSize.length - 1);
+        }
+    }
+    handleNavFirst(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.first.href);
+    }
+    handleNavPrev(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.prev.href);
+    }
+    handleNavNext(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.next.href);
+    }
+    handleNavLast(e) {
+        e.preventDefault();
+        this.props.onNavigate(this.props.links.last.href);
+    }
     render() {
         var users = this.props.users.map(user =>
             <User key={user._links.self.href} user={user}/>
         );
+
+        var navLinks = [];
+        if ("first" in this.props.links) {
+            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+        }
+        if ("prev" in this.props.links) {
+            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+        }
+        if ("next" in this.props.links) {
+            navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+        }
+        if ("last" in this.props.links) {
+            navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+        }
         return (
             <table>
                 <tbody>
                 <tr>
                     <th>Email</th>
+                    <th>Delete</th>
                 </tr>
                 {users}
                 </tbody>
@@ -61,21 +149,74 @@ export class UsersList extends Component {
 }
 
 export class User extends Component {
+    handleDelete() {
+        this.props.onDelete(this.props.user);
+    }
     render() {
         return (
             <tr>
                 <td>{this.props.user.email}</td>
+                <td>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </td>
             </tr>
         )
     }
 }
 
 export class App extends Component {
-  render() {
+    render() {
     return (
       <div>
         <UserApi />
       </div>
     );
-  }
+    }
+}
+
+export class CreateDialog extends Component {
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var newUser = {};
+        this.props.attributes.forEach(attribute => {
+            newUser[attribute] = React.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newUser);
+
+        // clear out the dialog's inputs
+        this.props.attributes.forEach(attribute => {
+            React.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        // Navigate away from the dialog to hide it.
+        window.location = "#";
+    }
+    render() {
+        var inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+                <input type="text" placeholder={attribute} ref={attribute} className="field" />
+            </p>
+        );
+
+        return (
+            <div>
+                <a href="#createUser">Create</a>
+
+                <div id="createUser" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+
+                        <h2>Create new user</h2>
+
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
 }
