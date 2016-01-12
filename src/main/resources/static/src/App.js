6fb9694f5973efe0cea7cd5cc8d0b4eefdcdf3aa
Zygimantas Benetis <benetis.zygimantas@gmail.com>
2016-01-12 16:14:06 +0200

    fetching eTags for each resource individually for update

--- src/main/resources/static/src/App.js
+++ src/main/resources/static/src/App.js
@@ -28,6 +28,15 @@ export class UserApi extends Component {
                 this.schema = schema.entity;
                 return userCollection;
             });
+        }).then(userCollection => {
+            return userCollection.entity._embedded.users.map(user =>
+                client({
+                    method: 'GET',
+                    path: user._links.self.href
+                })
+            );
+        }).then(usersPromises => {
+            return when.all(usersPromises);
         }).done(userCollection => {
             this.setState({
                 users: userCollection.entity._embedded.users,
@@ -219,6 +228,49 @@ export class CreateDialog extends Component {
                         <form>
                             {inputs}
                             <button onClick={this.handleSubmit.bind(this)}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export class UpdateDialog extends Component {
+    handleSubmit(e) {
+        e.preventDefault();
+        var updatedUser = {};
+        this.props.attributes.forEach(attribute => {
+            updatedUser[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onUpdate(this.props.user, updatedUser);
+        window.location = "#";
+    }
+
+    render() {
+        var inputs = this.props.attributes.map(attribute =>
+            <p key={this.props.user.entity[attribute]}>
+                <input type="text" placeholder={attribute}
+                       defaultValue={this.props.user.entity[attribute]}
+                       ref={attribute} className="field" />
+            </p>
+        );
+
+        var dialogId = "updateUser-" + this.props.user.entity._links.self.href;
+
+        return (
+            <div key={this.props.user.entity._links.self.href}>
+                <a href={"#" + dialogId}>Update</a>
+                <div id={dialogId} className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+
+                        <h2>Update user</h2>
+
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Update</button>
                         </form>
                     </div>
                 </div>
